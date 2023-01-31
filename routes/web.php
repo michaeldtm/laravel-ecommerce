77e3adcd1096add8f693cd8af1b883d34c129cf3
Michael Torres <mtorresm.094@gmail.com>
2023-01-31 04:52:41 -0500

    move register route from api to web route file

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Auth\RegisteredUserController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -12,6 +13,10 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+
+Route::post('/register', [RegisteredUserController::class, 'store'])
+    ->middleware('guest')
+    ->name('register');
 
 Route::get('/', function () {
     return view('welcome');
