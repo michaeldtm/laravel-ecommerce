ac8657aec52a9edd720c71565855843c551cfc68
Michael Torres <mtorresm.094@gmail.com>
2023-02-09 23:51:10 -0500

    refactor store method implementing DB::transaction

--- app/Http/Controllers/Api/ProductController.php
+++ app/Http/Controllers/Api/ProductController.php
@@ -6,8 +6,10 @@ use App\Http\Controllers\Controller;
 use App\Http\Requests\ProductRequest;
 use App\Http\Resources\ProductResource;
 use App\Models\Product;
+use App\Models\User;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
+use Illuminate\Support\Facades\DB;
 
 class ProductController extends Controller
 {
@@ -22,15 +24,19 @@ class ProductController extends Controller
 
     public function store(ProductRequest $request): JsonResponse
     {
+        /** @var User $user */
         $user = $request->user();
 
-        $product = $user->products()->save(
-            new Product([...$request->validated()])
-        );
+        DB::transaction(function () use ($request, $user) {
+            /** @var Product $product */
+            $product = $user->products()->save(
+                new Product([...$request->validated()])
+            );
 
-        $product->categories()->sync(
-            $request->get('categories')
-        );
+            $product->categories()->sync($request->get('categories'));
+
+            return $product;
+        });
 
         return response()->json([
             'message' => __('messages.product.created')
